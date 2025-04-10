fe94b8cf9a0ee7b5d6f9b227aa131a1210a550b0
Samantha Jabbour <sammiejab@hotmail.com>
2025-04-10 02:21:58 -0400

    trying to fix leaderboard v2

--- cron/slippi.ts
+++ cron/slippi.ts
@@ -1,6 +1,50 @@
 import { RateLimiter } from "limiter"
-
 export const getPlayerData = async (connectCode: string) => {
+  const query = `
+    query {
+      getConnectCode(code: "${connectCode.toUpperCase()}") {
+        user {
+         fbUid
+          displayName
+          connectCode {
+             code
+           }
+        status
+        activeSubscription {
+            level
+            hasGiftSub
+        }
+        rankedNetplayProfile {
+            id
+            ratingOrdinal
+            ratingUpdateCount
+            wins
+            losses
+            dailyGlobalPlacement
+            dailyRegionalPlacement
+           continent
+           characters {
+              character
+                gameCount
+            }
+        }
+            
+}}}`;
+
+  const req = await fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+    method: 'POST',
+  });
+
+  const json = await req.json();
+  return json;
+};
+
+
+/*export const getPlayerData = async (connectCode: string) => {
   const query = `fragment profileFieldsV2 on NetplayProfileV2 {
     id
     ratingOrdinal
@@ -54,9 +98,9 @@ export const getPlayerData = async (connectCode: string) => {
     method: 'POST',
   });
   return req.json();
-};
+};*/
 
-const limiter = new RateLimiter({tokensPerInterval: 1, interval: 'second'})
+const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 'second' })
 
 export const getPlayerDataThrottled = async (connectCode: string) => {
   const remainingRequests = await limiter.removeTokens(1);
