d658d4c7679df2153f67f4328c532560149cfcd1
Samantha Jabbour <sammiejab@hotmail.com>
2025-04-09 20:13:27 -0400

    daily gm limit

--- src/components/routes/GmRank.tsx
+++ src/components/routes/GmRank.tsx
@@ -68,7 +68,7 @@ export default function GmRank() {
   const [inputCode, setInputCode] = useState("");
   const [opponentCode, setOpponentCode] = useState<string | null>(null);
 
-  const playerCode = "FOXN#463"; // Your main player
+  const playerCode = "THEC#722"; // Your main player
   const options: Options = {
     tau: TAU,
     mu: 25,
@@ -123,7 +123,7 @@ export default function GmRank() {
 
   return (
     <div style={{ padding: "2rem", fontFamily: "monospace", fontSize: "1.3rem", textAlign: "center" }}>
-      <h2>Slippi Rating Predictor for I Be Doin Weed</h2>
+      <h2>Slippi Rating Predictor for The Crimson Slur</h2>
 
       <div style={{ marginBottom: "1rem" }}>
         <label>
