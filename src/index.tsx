9a16b0c352362695d2018881daad6f83eb58e319
Samantha Jabbour <sammiejab@hotmail.com>
2025-04-10 02:44:44 -0400

    trying to fix leaderboard v2

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import './index.css';
-
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
 render(<App />, document.getElementById('app'));
