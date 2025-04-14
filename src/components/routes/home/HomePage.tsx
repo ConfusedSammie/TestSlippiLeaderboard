4c8b0605123a8b4dcac6d938007a3b89688d6cce
Samantha Jabbour <sammiejab@hotmail.com>
2025-04-13 21:27:05 -0400

    trying to fix leaderboard v2

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -57,7 +57,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col items-center h-screen p-8">
       <h1 className="text-3xl m-4 text-center text-white">
-        YOU NEED 2315.5 FOR GRANDMASTER ON 12TH APRIL 2025
+        YOU NEED 2315.5 FOR GRANDMASTER ON 13TH APRIL 2025
         </h1>
     
       <img className="h-48" src={ColoradoFlag} alt="colorado flag" />
