a9af884e1a533a8e181a4e32a9d314da8687995e
Samantha Jabbour <sammiejab@hotmail.com>
2025-05-05 16:41:00 -0400

    turned into a luigi leaderboard

--- cron/run.sh
+++ cron/run.sh
@@ -2,4 +2,4 @@
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.."
 /home/confusedsammie/.nvm/versions/node/v18.12.0/bin/node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
-echo "$(date) - run.sh executed" >> /tmp/cron_test.log
+echo "$(date) - run.sh executed" >> logs/cron_test.log
